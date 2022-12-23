78164b903e2c372b8afd73759068077d608bdaed
PramodKumarHK89 <pramkum@microsoft.com>
2022-12-23 13:39:25 +0530

    Updating configs

--- Python/PythonWebApp-ADAL-To-MSAL/1.Before-webapp-ADAL/config.py
+++ Python/PythonWebApp-ADAL-To-MSAL/1.Before-webapp-ADAL/config.py
@@ -1,8 +1,8 @@
 RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "pramkumlab.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+TENANT = "Your tenant"  # Enter tenant name, e.g. contoso.onmicrosoft.com
 AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "5362bf98-2b62-4ad9-b9fa-d787b0b92ead"  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = ".Bm8Q~4rd4nv.B2dO69iYDDdyYgy.d-HodshmcLW"  # copy the value of key you generated when setting up the application
+CLIENT_ID = "Your client id "  # copy the Application ID of your app from your Azure portal
+CLIENT_SECRET = "Your client secret"  # copy the value of key you generated when setting up the application
 
 # These settings are for the Microsoft Graph API Call
 API_VERSION = 'v1.0'
